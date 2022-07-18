d842d527f7ef8a5fc046ab771d1574138a9db25b
Timothy Gaither <timjgaither@gmail.com>
2022-07-18 17:53:07 +0200

    push pizzeria post

--- Models/Pizza.cs
+++ Models/Pizza.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using la_mia_pizzeria_static.ValidationAttributes;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 [Table("pizza")]
@@ -6,9 +7,15 @@ public class Pizza
     {
         [Key]
         public int PizzaId { get; set; }
+        [StringLength(20, ErrorMessage ="Il numero massimo di caratteri inseribili è 20 caratteri")]
+        [Required(ErrorMessage ="Il campo è obbligatorio")]
         public string Name { get; set; }
+        [ValidatorPizzaForm]
+        [StringLength(100, ErrorMessage = "Il numero massimo di caratteri inseribili è 100 caratteri")]
         public string Description { get; set; }
+       
         public string Img { get; set; }
+        [Range(1, 100, ErrorMessage = "Il prezzo selezionato non è valido, min 1/ max 100")]
         public double Price { get; set; }
     public Pizza()
     {
---
+++ ValidationAttributes/ValidatorPizzaForm.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace la_mia_pizzeria_static.ValidationAttributes
+{
+
+    public class ValidatorPizzaForm : ValidationAttribute
+    {
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            string fieldValue = (string)value;
+            if (fieldValue == null || fieldValue.Trim().IndexOf(" ") == -1)
+            {
+                return new ValidationResult("Il campo deve contenere almeno 5 parole");
+            }
+            return ValidationResult.Success;
+        }
+    }
+}
